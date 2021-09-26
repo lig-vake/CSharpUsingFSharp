b9ea1fc4a771370b3aa4274b4a25e64ee3c1b86e
Andrey Vlasyev <vlasyevandrei@gmail.com>
2021-09-26 21:53:31 +0300

    Добавил все файлы

---
+++ CSharpUsingFSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpUsingFSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayToSort = new int[] { 4, 19, 1, 45, 8, 33};
+            Array.ForEach(FSharpCode.FSharpSort.sort(arrayToSort), Console.WriteLine);
+
+            Console.ReadKey();
+        }
+    }
+}
